721c7d7f74a64d567e9062aed82cdd94a8b6f204
selvaganapathi.g <selvaganapathi.g@IMPC1952.htcindia.local>
2019-07-19 20:15:45 +0530

    listeners added

--- src/com/servlet/Hello.java
+++ src/com/servlet/Hello.java
@@ -2,11 +2,15 @@ package com.servlet;
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Hello
@@ -26,11 +30,18 @@ public class Hello extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext ctx = request.getServletContext();
+		ctx.setAttribute("User", "Pankaj");
+		String user = (String) ctx.getAttribute("User");
+		ctx.removeAttribute("User");
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		PrintWriter out = response.getWriter();
+		out.write("Hi "+user);
+}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
